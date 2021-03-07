ef038cec643d901a644b127a29f9e117336a2214
muhammadlovlu <chyshahab@gmail.com>
2021-03-07 19:36:19 +0600

    updated

---
+++ dist/typescript.js
@@ -0,0 +1,14 @@
+"use strict";
+let myMoney = 50;
+myMoney = 40;
+myMoney = 0;
+let age = 50;
+age = 30;
+let club = 'Venizuella';
+const famous = false;
+let newFamous;
+newFamous = true;
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(5, 75);
---
+++ typescript.ts
@@ -0,0 +1,27 @@
+let myMoney = 50;
+myMoney = 40;
+myMoney = 0; 
+
+
+let age:number = 50;
+age = 30;
+let club:string = 'Venizuella';
+const famous: boolean = false;
+let newFamous:boolean;
+newFamous = true;
+
+
+
+function add(num1:number ,num2:number){
+    return num1 + num2;
+
+}
+add(5,75);
+add(44, 98);
+
+function fullName (firstName :string, lastName:string): string{
+    return firstName + ' ' + lastName;
+}
+
+
+const userOne =fullName('Mack', 'LOren');
